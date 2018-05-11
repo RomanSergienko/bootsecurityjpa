1629a2a97e9a9f7052507a512d60e4292135bb74
Roman Sergienko <sergienko@tzoom.pro>
2018-05-11 09:53:53 +0400

    Initial commit

---
+++ src/main/java/ru/sergienko/bootsecurityjpa/BootsecurityjpaApplication.java
@@ -0,0 +1,12 @@
+package ru.sergienko.bootsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsecurityjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootsecurityjpaApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/sergienko/bootsecurityjpa/BootsecurityjpaApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.sergienko.bootsecurityjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootsecurityjpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
